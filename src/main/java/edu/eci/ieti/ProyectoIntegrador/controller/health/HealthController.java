694b6b64ff523eabb72c675c6b8cee5648819283
SanRocks1220 <xxsantirocha12xx@gmail.com>
2024-02-01 20:57:47 -0500

    Configuración inicial completa

---
+++ src/main/java/edu/eci/ieti/ProyectoIntegrador/controller/health/HealthController.java
@@ -0,0 +1,16 @@
+package edu.eci.ieti.ProyectoIntegrador.controller.health;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @RequestMapping("/health")
+
+    public String checkAPI() {
+
+        return "<h1>The API is working ok!</h1>";
+
+    }
+}
---
+++ src/test/java/edu/eci/ieti/ProyectoIntegrador/ProyectoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.ProyectoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
